b0ef9153a9ecc8504f5bd5c20e0605b2067f0e23
Matilde Morrone <contact@morrone.dev>
2024-10-19 20:35:42 +0200

    Generate more bindings

--- spidermonkey-sys/src/lib.rs
+++ spidermonkey-sys/src/lib.rs
@@ -4,6 +4,33 @@ include_cpp! {
     #include "spidermonkey.hpp"
     // safety!(unsafe_ffi)
     generate!("JS_Init")
+    generate!("JS_NewContext")
+    generate!("JS::DefaultHeapMaxBytes")
+    generate!("JS_DestroyContext")
+    generate!("JS_ShutDown")
+    generate!("JS::RealmOptions")
+    generate_pod!("JSClass")
+    generate!("JSCLASS_GLOBAL_FLAGS")
+    generate!("JS::DefaultGlobalClassOps")
+    // generate!("JS_NewEnumerateStandardClasses")
+    // generate!("JS_ResolveStandardClass")
+    // generate!("JS_MayResolveStandardClass")
+    // generate!("JS_GlobalObjectTraceHook")
 }
 
+unsafe impl Sync for JSClass {}
+
 pub use ffi::*;
+
+// const JSClassOps JS::DefaultGlobalClassOps = {
+//     nullptr,                         // addProperty
+//     nullptr,                         // delProperty
+//     nullptr,                         // enumerate
+//     JS_NewEnumerateStandardClasses,  // newEnumerate
+//     JS_ResolveStandardClass,         // resolve
+//     JS_MayResolveStandardClass,      // mayResolve
+//     nullptr,                         // finalize
+//     nullptr,                         // call
+//     nullptr,                         // construct
+//     JS_GlobalObjectTraceHook,        // trace
+// };
