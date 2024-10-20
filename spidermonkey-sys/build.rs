d7458247f8cb20746e4d8ee447c4175379b288c3
Federico Maria Morrone <contact@morrone.dev>
2024-10-20 18:00:02 +0200

    Refactor build script

--- spidermonkey-sys/build.rs
+++ spidermonkey-sys/build.rs
@@ -1,20 +1,12 @@
-use std::path::PathBuf;
-
 use anyhow::Result;
 
 fn main() -> Result<()> {
-    let path = PathBuf::from("src");
-    let mut b = autocxx_build::Builder::new("src/lib.rs", &[&path])
-        .extra_clang_args(&["-I/usr/include/mozjs-128", "-std=c++20", "-DDEBUG=1"])
-        .build()?;
-
-    b.flag_if_supported("-std=c++20")
+    autocxx_build::Builder::new("src/lib.rs", &["src"])
+        .extra_clang_args(&["-I/usr/include/mozjs-128", "-std=c++20"])
+        .build()?
+        .flag("-std=c++20")
         .include("/usr/include/mozjs-128")
-        .flag("-DDEBUG=1")
         .compile("spidermonkey-sys");
-
-    println!("cargo:rerun-if-changed=src/lib.rs");
-    println!("cargo:rerun-if-changed=src/spidermonkey.hpp");
 
     pkg_config::probe_library("mozjs-128")?;
 
