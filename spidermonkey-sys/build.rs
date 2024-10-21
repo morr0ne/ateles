3df797be7167bf5de3da9082cdb14da2a370f213
Matilde Morrone <contact@morrone.dev>
2024-10-21 15:26:44 +0200

    Suppress warnings in build script

--- spidermonkey-sys/build.rs
+++ spidermonkey-sys/build.rs
@@ -2,13 +2,14 @@ use anyhow::Result;
 
 fn main() -> Result<()> {
     autocxx_build::Builder::new("src/lib.rs", &["src"])
-        .extra_clang_args(&["-I/usr/include/mozjs-128", "-std=c++20"])
+        .extra_clang_args(&["-I/usr/include/mozjs-128", "-std=c++20", "-w"])
         .build()?
         .flag("-std=c++20")
+        .flag("-w")
         .include("/usr/include/mozjs-128")
         .compile("spidermonkey-sys");
 
-    pkg_config::probe_library("mozjs-128")?;
+    pkg_config::Config::new().statik(true).probe("mozjs-128")?;
 
     Ok(())
 }
